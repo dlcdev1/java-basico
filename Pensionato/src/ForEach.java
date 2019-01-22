fe24d19772d898b00b67cd79ca7388d1b6452167
dlcdev1 <dlclegal@hotmail.com>
2019-01-22 16:58:53 -0200

    novos exemplos

---
+++ Pensionato/src/ForEach.java
@@ -0,0 +1,13 @@
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		String[] vect = new String[] {"Maria", "Bob", "Alex"};
+		
+		for (String obj : vect) {
+			System.out.println(obj);
+		}
+		
+	}
+
+}
